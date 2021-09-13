99893a2bfda5cb098fad0c20cbb74d39c30d74fb
AdisBosnjak <43806391+Bozzeh@users.noreply.github.com>
2021-09-13 20:59:23 +1000

    P03 - Data Pagination

---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>POSTCODE</th>
+                <th>TYPE</th>
+                <th>COMMENTS</th>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: [ 'enquiries' ],
+    data () {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+</style>
